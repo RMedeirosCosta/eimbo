1f4e7988afa6e77262d29478a84910d68fb004ea
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2015-08-17 01:31:54 -0300

    duplicated

--- projects/csharp/Eimbo/Eimbo.Dominio/Cadastro/Excecao/ExcecaoDescricaoFormaPagamentoJaCadastrada.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Eimbo.Dominio.Cadastro.Excecao
-{
-    public class ExcecaoDescricaoFormaPagamentoJaCadastrada :Exception
-    {
-        public ExcecaoDescricaoFormaPagamentoJaCadastrada() : base("Essa descrição já está cadastrada para outra forma de pagamento!") { }
-    }
-}
--- projects/csharp/Eimbo/Eimbo.Dominio/Cadastro/Excecao/ExcecaoDescricaoServicoJaCadastrada.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Eimbo.Dominio.Cadastro.Excecao
-{
-    public class ExcecaoDescricaoServicoJaCadastrada :Exception
-    {
-        public ExcecaoDescricaoServicoJaCadastrada() : base("Essa descrição já está cadastrada para outro serviço!") { }
-    }
-}
